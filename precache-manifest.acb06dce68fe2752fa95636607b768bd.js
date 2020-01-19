aeefffd9496a8de881c31f39df47681ed44cf7c6
Deepak Bhardwaj <deepak@customersuccessbox.com>
2020-01-19 17:40:45 +0530

    Updates

---
+++ precache-manifest.acb06dce68fe2752fa95636607b768bd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5dcd30109b5acfee400d39809a19f133",
+    "url": "/vidly/index.html"
+  },
+  {
+    "revision": "3da13aab0b3e93843505",
+    "url": "/vidly/static/css/2.ff8bd605.chunk.css"
+  },
+  {
+    "revision": "6c290fd2a66eb9884d80",
+    "url": "/vidly/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "3da13aab0b3e93843505",
+    "url": "/vidly/static/js/2.101286e2.chunk.js"
+  },
+  {
+    "revision": "b36213931fb851fe04ff128b9d6a2ba0",
+    "url": "/vidly/static/js/2.101286e2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6c290fd2a66eb9884d80",
+    "url": "/vidly/static/js/main.ec10b6a3.chunk.js"
+  },
+  {
+    "revision": "28c30ed05b03eddd56ea",
+    "url": "/vidly/static/js/runtime-main.66905aeb.js"
+  }
+]);
