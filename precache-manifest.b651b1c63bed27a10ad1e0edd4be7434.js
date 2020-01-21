b494fcc62d018bb3e0ee730dbec66210f70f9e6a
Deepak Bhardwaj <deepak@customersuccessbox.com>
2020-01-22 00:01:59 +0530

    Updates

--- precache-manifest.b651b1c63bed27a10ad1e0edd4be7434.js
+++ precache-manifest.b651b1c63bed27a10ad1e0edd4be7434.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "7164ce12fdb37b6aefa40dd23be242f6",
+    "revision": "e2b1fb583238c8034e101e2c24588ada",
     "url": "/vidly/index.html"
   },
   {
-    "revision": "9b3040e9c651c6ffc444",
+    "revision": "dfa3afbcc59360f5e61d",
     "url": "/vidly/static/css/2.ff8bd605.chunk.css"
   },
   {
-    "revision": "394f804ceb89395a7fad",
+    "revision": "70e76f47f3ba4e3a564a",
     "url": "/vidly/static/css/main.5ecd60fb.chunk.css"
   },
   {
-    "revision": "9b3040e9c651c6ffc444",
-    "url": "/vidly/static/js/2.b0d0f902.chunk.js"
+    "revision": "dfa3afbcc59360f5e61d",
+    "url": "/vidly/static/js/2.51aaaf2d.chunk.js"
   },
   {
     "revision": "3eb958307e27ec694eb41bda990a4177",
-    "url": "/vidly/static/js/2.b0d0f902.chunk.js.LICENSE"
+    "url": "/vidly/static/js/2.51aaaf2d.chunk.js.LICENSE"
   },
   {
-    "revision": "394f804ceb89395a7fad",
-    "url": "/vidly/static/js/main.df79ebb5.chunk.js"
+    "revision": "70e76f47f3ba4e3a564a",
+    "url": "/vidly/static/js/main.68d8106c.chunk.js"
   },
   {
     "revision": "28c30ed05b03eddd56ea",
